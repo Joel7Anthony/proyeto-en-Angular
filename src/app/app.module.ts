8babf8e7e3ce4bc4971305a63c242122ca5032b6
Joel7Anthony <anto777molina777@gmail.com>
2022-12-14 16:02:36 -0500

    carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductComponent } from './pages/product/product.component';
+import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { NavLeftComponent } from './shared/nav-left/nav-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CategoryComponent,
+    DashboardComponent,
+    ProductComponent,
+    BreadcumbsComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    NavLeftComponent
   ],
   imports: [
     BrowserModule
